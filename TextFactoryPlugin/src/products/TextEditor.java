647f8094a2ae7da71d05ed747e63b244d16236a7
loressl <loryssl@hotmail.com>
2019-07-28 10:16:43 -0300

    Atividade 4: implementação do adater para abrir arquivos pdf.

--- TextFactoryPlugin/src/products/TextEditor.java
+++ TextFactoryPlugin/src/products/TextEditor.java
@@ -1,11 +1,23 @@
 package products;
 
+import java.io.File;
+
+import adapter.TextDocumentAdapter;
+import interfaces.IDocumentAdapter;
 import interfaces.IDocumentEditor;
 
 public class TextEditor implements IDocumentEditor{
-
+	
+	public TextEditor() {
+		this.documentAdapter = new TextDocumentAdapter();
+	}
+	
 	@Override
-	public void open() {
+	public void open(String typeExtension, File file) {
+		documentAdapter.setAdaptee(typeExtension);
+		documentAdapter.openDocument(file);
 		System.out.println("Editor de texto aberto com sucesso!!");
 	}
+
+	private IDocumentAdapter documentAdapter;
 }
